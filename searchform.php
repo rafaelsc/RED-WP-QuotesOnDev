731c49a5bfdced7802349f34168f880b48ee5828
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-06-02 15:07:29 -0700

    Update CSS

--- searchform.php
+++ searchform.php
@@ -1,8 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
-		<label>
-			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
-		</label>
+		<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		<button class="search-submit">
 			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
 			<i class="fas fa-search"></i>
